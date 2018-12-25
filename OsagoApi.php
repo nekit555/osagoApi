8191c859506b8a0c64a059d200a0b9a3f8c9861e
nekit555 <nekitpetrouch@gmail.com>
2018-12-25 23:39:29 +0500

    Update OsagoApi.php

--- OsagoApi.php
+++ OsagoApi.php
@@ -56,27 +56,6 @@ class OsagoApi
     function curl_post($url, array $post = NULL, array $options = [])
     {
         $api_url = $this->url . $url;
-        $defaults = [
-            CURLOPT_POST => 1,
-            CURLOPT_HEADER => 0,
-            CURLOPT_URL => $api_url,
-            CURLOPT_FRESH_CONNECT => 1,
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_FORBID_REUSE => 1,
-            CURLOPT_TIMEOUT => 4
-        ];
-        if (!is_null($post)) {
-            $defaults += [ CURLOPT_POSTFIELDS => http_build_query($post) ];
-        }
-
-        $ch = curl_init();
-        curl_setopt_array($ch, ($options + $defaults));
-        if( ! $result = curl_exec($ch))
-        {
-            trigger_error(curl_error($ch));
-        }
-        curl_close($ch);
-        return $result;
     }
 
     /**
